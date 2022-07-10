7910b6221a0b3fd0c3d6beb98046ddfd5014b137
yaotyrone@gmail.com <yaotyrone@gmail.com>
2022-07-11 00:56:05 +0200

    Initial project

---
+++ src/main/java/com/dev/testingapp/TestingAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev.testingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingAppApplication.class, args);
+	}
+
+}
